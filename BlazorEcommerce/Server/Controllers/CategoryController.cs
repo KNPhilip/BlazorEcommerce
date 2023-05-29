a7b0e0dc4b26192cdf75da1b35b3291a7a676367
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 11:20:25 +0200

    Created CategoryController and used the service to get categories.

---
+++ BlazorEcommerce/Server/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
+        {
+            var result = await _categoryService.GetCategoriesAsync();
+            return Ok(result);
+        }
+    }
+}
