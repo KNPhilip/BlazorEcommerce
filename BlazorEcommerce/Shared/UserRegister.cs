d756a79eb0d42fb4923a46b6066dc3a04345d4a7
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 22:41:41 +0200

    Added front-end stuff on the Register page.

--- BlazorEcommerce/Shared/UserRegister.cs
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -2,7 +2,7 @@
 {
     public class UserRegister
     {
-        public string Ema { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string ConfirmPassword { get; set; } = string.Empty;
     }
