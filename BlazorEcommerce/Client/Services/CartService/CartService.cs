99cbeb8f002b493ce7d078a9f9b0132ac20f006a
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 20:58:27 +0200

    Added the correct number of items in the Cart Counter.

--- BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -21,6 +21,7 @@
             cart.Add(cartItem);
 
             await _localStorage.SetItemAsync("cart", cart);
+            OnChange.Invoke();
         }
 
         public async Task<List<CartItem>> GetCartItems()
