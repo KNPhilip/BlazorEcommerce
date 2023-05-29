a01a7fcbb817de092c6a7a2b4b35672f6b15657b
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 11:06:07 +0200

    Seeded more products.

--- BlazorEcommerce/Server/Data/EcommerceContext.cs
+++ BlazorEcommerce/Server/Data/EcommerceContext.cs
@@ -9,7 +9,7 @@
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>().HasData(
-                new Product
+                new
                 {
                     Id = 1,
                     Title = "The Hitchhiker's Guide to the Galaxy",
@@ -18,7 +18,7 @@
                     Price = 9.99m,
                     CategoryId = 1
                 },
-                new Product
+                new
                 {
                     Id = 2,
                     Title = "Ready Player One",
@@ -27,7 +27,7 @@
                     Price = 7.99m,
                     CategoryId = 1
                 },
-                new Product
+                new
                 {
                     Id = 3,
                     Title = "Nineteen Eighty-Four",
@@ -35,23 +35,95 @@
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
                     Price = 6.99m,
                     CategoryId = 1
+                },
+                new
+                {
+                    Id = 4,
+                    CategoryId = 2,
+                    Description = "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis, and produced by Joel Silver. Starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and as the first installment in the Matrix franchise, it depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, which intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias \"Neo\", uncovers the truth, he \"is drawn into a rebellion against the machines\" along with other people who have been freed from the Matrix.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+                    Price = 4.99m,
+                    Title = "The Matrix"
+                },
+                new
+                {
+                    Id = 5,
+                    CategoryId = 2,
+                    Description = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett \"Doc\" Brown (Lloyd). Trapped in the past, Marty inadvertently prevents his future parents' meeting—threatening his very existence—and is forced to reconcile the pair and somehow get back to the future.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+                    Price = 3.99m,
+                    Title = "Back to the Future"
+                },
+                new
+                {
+                    Id = 6,
+                    CategoryId = 2,
+                    Description = "Toy Story is a 1995 American computer-animated comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The first installment in the Toy Story franchise, it was the first entirely computer-animated feature film, as well as the first feature film from Pixar. The film was directed by John Lasseter (in his feature directorial debut), and written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, was produced by Bonnie Arnold and Ralph Guggenheim, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, the plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner, Andy Davis, to friends who work together to be reunited with Andy after being separated from him.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                    Price = 2.99m,
+                    Title = "Toy Story"
+                },
+                new
+                {
+                    Id = 7,
+                    CategoryId = 3,
+                    Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
+                    Price = 49.99m,
+                    Title = "Half-Life 2"
+                },
+                new
+                {
+                    Id = 8,
+                    CategoryId = 3,
+                    Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
+                    Price = 9.99m,
+                    Title = "Diablo II"
+                },
+                new
+                {
+                    Id = 9,
+                    CategoryId = 3,
+                    Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
+                    Price = 14.99m,
+                    Title = "Day of the Tentacle"
+                },
+                new
+                {
+                    Id = 10,
+                    CategoryId = 3,
+                    Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
+                    Price = 159.99m,
+                    Title = "Xbox"
+                },
+                new
+                {
+                    Id = 11,
+                    CategoryId = 3,
+                    Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
+                    Price = 79.99m,
+                    Title = "Super Nintendo Entertainment System"
                 }
             );
 
             modelBuilder.Entity<Category>().HasData(
-                new Category
+                new
                 {
                     Id = 1,
                     Name = "Books",
                     Url = "books"
                 },
-                new Category
+                new
                 {
                     Id = 2,
                     Name = "Movies",
                     Url = "movies"
                 },
-                new Category
+                new
                 {
                     Id = 3,
                     Name = "Video Games",
