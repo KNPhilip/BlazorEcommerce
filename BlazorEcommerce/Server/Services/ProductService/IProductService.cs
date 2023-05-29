13eead0ebbf505c969bcc423690d422f65adfa54
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 16:08:15 +0200

    Added method for product searching in the server-side services.

--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -5,5 +5,6 @@
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
         Task<ServiceResponse<List<Product>>> GetProductsByCategoryAsync(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -53,5 +53,20 @@
 
             return response;
         }
+
+        public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products
+                    .Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+                    ||
+                    p.Description.ToLower().Contains(searchText.ToLower()))
+                    .Include(p => p.Variants)
+                    .ToListAsync()
+            };
+
+            return response;
+        }
     }
 }
