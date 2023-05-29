e036184aef9fea657ff000e5c603bf046e7f7b3b
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 22:23:26 +0200

    Styled and added update quantity functionallity on the Cart page.

--- BlazorEcommerce/Shared/CartItem.cs
+++ BlazorEcommerce/Shared/CartItem.cs
@@ -4,6 +4,6 @@
     {
         public int ProductId { get; set; }
         public int ProductTypeId { get; set; }
-        public int Quantity { get; set; }
+        public int Quantity { get; set; } = 1;
     }
 }
