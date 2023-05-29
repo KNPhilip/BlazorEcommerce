e0d34f114d34be3b81006ff9ba879b54e6a7785d
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 12:42:06 +0200

    Implemented categories in the NavMenu.razor

--- BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -9,7 +9,7 @@
             _http = http;
         }
 
-        public List<Category>? Categories { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category>();
 
         public async Task GetCategories()
         {
