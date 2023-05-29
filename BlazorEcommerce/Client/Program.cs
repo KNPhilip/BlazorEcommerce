462661c75f2d4fbcb60e41875a5fa9abe68f43a6
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 20:51:22 +0200

    Implemented CartItem service (DI/IoC) and Created "Add to Cart" button & functionality.

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -16,5 +16,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
