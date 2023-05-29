f1ce0a0dd3b9ea868647a52aff88c7d9ce936d3e
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 21:02:33 +0200

    Added CartProductResponseDto

---
+++ BlazorEcommerce/Shared/Dtos/CartProductResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Shared.Dtos
+{
+    public class CartProductResponseDto
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int ProductTypeId { get; set; }
+        public string ProductType { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
