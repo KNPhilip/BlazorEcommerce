e09e5fcf16178222cfaffe492697be5b1e82af7f
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 15:31:11 +0200

    Modified methods in the server-side services to include ProductVariants & then ProductTypes.

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -12,7 +12,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
@@ -30,7 +33,10 @@
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products
+                    .Include(p => p.Variants)
+                    .ThenInclude(v => v.ProductType)
+                    .ToListAsync()
             };
 
             return response;
