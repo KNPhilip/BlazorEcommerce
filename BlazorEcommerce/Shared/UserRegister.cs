5cd9e2c9fff47e0e34d0c7d80d816ffe34aa09e2
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 22:56:18 +0200

    Added Validators to the UserRegister model.

--- BlazorEcommerce/Shared/UserRegister.cs
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -1,9 +1,14 @@
-﻿namespace BlazorEcommerce.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Shared
 {
     public class UserRegister
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
