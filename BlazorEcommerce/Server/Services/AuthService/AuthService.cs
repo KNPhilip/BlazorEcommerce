46f85e2b502da0f8ae443c741e39196f6cf8324d
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 23:13:44 +0200

    Added method for looking up user by email in the server-side AuthService.

--- BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -2,5 +2,21 @@
 {
     public class AuthService : IAuthService
     {
+        private readonly EcommerceContext _context;
+
+        public AuthService(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            return await _context.Users.AnyAsync(user => user.Email.ToLower().Equals(email.ToLower()));
+        }
     }
 }
--- BlazorEcommerce/Server/Services/AuthService/IAuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/IAuthService.cs
@@ -2,5 +2,6 @@
 {
     public interface IAuthService
     {
+        Task<bool> UserExists(string email);
     }
 }
