0e6be5e70f1879454a8901ba48f8acb1bd6e4d32
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 21:10:59 +0200

    Added CartService on the server-side along with a method to retrieve products from the cart.

---
+++ BlazorEcommerce/Server/Services/CartService/CartService.cs
@@ -0,0 +1,52 @@
+﻿namespace BlazorEcommerce.Server.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly EcommerceContext _context;
+
+        public CartService(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<List<CartProductResponseDto>>> GetCartProductsAsync(List<CartItem> cartItems)
+        {
+            var result = new ServiceResponse<List<CartProductResponseDto>>
+            {
+                Data = new List<CartProductResponseDto>()
+            };
+
+            foreach (var item in cartItems)
+            {
+                var product = await _context.Products
+                    .Where(p => p.Id == item.ProductId)
+                    .FirstOrDefaultAsync();
+
+                if (product is null)
+                    continue;
+
+                var productVariant = await _context.ProductVariants
+                    .Where(v => v.ProductId == item.ProductId && v.ProductTypeId == item.ProductTypeId)
+                    .Include(v => v.ProductType)
+                    .FirstOrDefaultAsync();
+
+                if (productVariant is null)
+                    continue;
+
+                var cartProduct = new CartProductResponseDto
+                {
+                    ProductId = product.Id,
+                    Title = product.Title,
+                    ImageUrl = product.ImageUrl,
+                    Price = productVariant.Price,
+                    ProductType = productVariant.ProductType.Name,
+                    ProductTypeId = productVariant.ProductTypeId,
+                };
+
+                result.Data.Add(cartProduct);
+            }
+
+            return result;
+        }
+    }
+}
