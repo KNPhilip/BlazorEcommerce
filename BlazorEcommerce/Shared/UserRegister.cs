019994d33ab38c20d8e3ec9871bdabdb3566d0a1
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-29 22:30:41 +0200

    Created UserRegister model.

---
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Shared
+{
+    internal class UserRegister
+    {
+        public string Ema { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
