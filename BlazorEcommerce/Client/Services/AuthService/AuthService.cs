a23afa2052bc161524f90d09701171614f6d9986
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-30 20:02:46 +0200

    Created method for login in the client-side AuthService.

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -9,6 +9,12 @@
             _http = http;
         }
 
+        public async Task<ServiceResponse<string>> Login(UserLoginDto request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/login", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
+        }
+
         public async Task<ServiceResponse<int>> Register(UserRegisterDto request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
