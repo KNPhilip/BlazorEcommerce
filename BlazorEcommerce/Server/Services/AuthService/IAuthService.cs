f98d78ffe4354b3849a174f5f183452eafda2b5e
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-01 22:47:48 +0200

    Created ChangePassword method in the server-side AuthService.

--- BlazorEcommerce/Server/Services/AuthService/IAuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/IAuthService.cs
@@ -7,5 +7,6 @@ namespace BlazorEcommerce.Server.Services.AuthService
         Task<ServiceResponse<int>> Register(User user, string password);
         Task<bool> UserExists(string email);
         Task<ServiceResponse<string>> Login(string email, string password);
+        Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword);
     }
 }
