3810a7abae799d851d73d4c41ba35439576f4590
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 23:22:10 +0200

    Implemented FulfillOrder in the PaymentController.

--- BlazorEcommerce/Server/Controllers/PaymentController.cs
+++ BlazorEcommerce/Server/Controllers/PaymentController.cs
@@ -19,5 +19,15 @@ namespace BlazorEcommerce.Server.Controllers
             var session = await _paymentService.CreateCheckoutSession();
             return Ok(session.Url);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<bool>>> FulfillOrder()
+        {
+            var response = await _paymentService.FulfillOrder(Request);
+            if (!response.Success)
+                return BadRequest(response.Message);
+
+            return Ok(response);
+        }
     }
 }
