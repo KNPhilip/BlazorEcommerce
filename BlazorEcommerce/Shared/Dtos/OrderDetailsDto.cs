bfb35feba16403c19566d8fe5c005df60fbb3c9e
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 18:01:24 +0200

    Created DTO's - OrderDetails and OrderDetailsProduct.

---
+++ BlazorEcommerce/Shared/Dtos/OrderDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared.Dtos
+{
+    internal class OrderDetailsDto
+    {
+    }
+}
