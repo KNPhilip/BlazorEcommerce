4e2fca993470ab7422ff69b502851ad040ba20e4
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 15:23:48 +0200

    Created OrderService on the client-side.

---
+++ BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorEcommerce.Client.Services.OrderService
+{
+    public interface IOrderService
+    {
+    }
+}
---
+++ BlazorEcommerce/Client/Services/OrderService/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Services.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _http;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly NavigationManager _navigationManager;
+
+        public OrderService(
+            HttpClient http,
+            AuthenticationStateProvider authStateProvider,
+            NavigationManager navigationManager )
+        {
+            _http = http;
+            _authStateProvider = authStateProvider;
+            _navigationManager = navigationManager;
+        }
+    }
+}
