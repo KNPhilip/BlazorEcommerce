cd3871b8f846d3df158b10cb9fa7bc3bc2e32c1a
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 18:12:59 +0200

    Added GetOrderDetails in the client-side OrderService.

--- BlazorEcommerce/Client/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/OrderService.cs
@@ -16,6 +16,12 @@
             _navigationManager = navigationManager;
         }
 
+        public async Task<OrderDetailsDto> GetOrderDetails(int orderId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<OrderDetailsDto>>($"api/order/{orderId}");
+            return result.Data;
+        }
+
         public async Task<List<OrderOverviewDto>> GetOrders()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<OrderOverviewDto>>>("api/order");
