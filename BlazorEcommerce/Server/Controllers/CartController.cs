eb7f152f7637cef5d2bd639e399b4d587f80ccb0
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 14:24:52 +0200

    Added UpdateQuantity to the CartController.

--- BlazorEcommerce/Server/Controllers/CartController.cs
+++ BlazorEcommerce/Server/Controllers/CartController.cs
@@ -32,6 +32,13 @@
             return Ok(result);
         }
 
+        [HttpPut("update-quantity")]
+        public async Task<ActionResult<ServiceResponse<bool>>> UpdateQuantity(CartItem cartItem)
+        {
+            var result = await _cartService.UpdateQuantity(cartItem);
+            return Ok(result);
+        }
+
         [HttpGet("count")]
         public async Task<ActionResult<ServiceResponse<int>>> GetCartItemsCount()
         {
