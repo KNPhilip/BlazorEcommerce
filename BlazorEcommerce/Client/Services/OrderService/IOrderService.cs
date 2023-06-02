7c6df8eceb962969d21a8e05dbeb4683cadcff74
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-02 22:09:37 +0200

    Modified client-side OrderService to work with Stripe.

--- BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
@@ -2,7 +2,7 @@
 {
     public interface IOrderService
     {
-        Task PlaceOrder();
+        Task<string> PlaceOrder();
         Task<List<OrderOverviewDto>> GetOrders();
         Task<OrderDetailsDto> GetOrderDetails(int orderId);
     }
--- BlazorEcommerce/Client/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/OrderService.cs
@@ -28,15 +28,17 @@
             return result.Data;
         }
 
-        public async Task PlaceOrder()
+        public async Task<string> PlaceOrder()
         {
             if (await IsUserAuthenticated())
             {
-                await _http.PostAsync("api/order", null);
+                var result = await _http.PostAsync("api/payment/checkout", null);
+                var url = await result.Content.ReadAsStringAsync();
+                return url;
             }
             else
             {
-                _navigationManager.NavigateTo("login");
+                return "login";
             }
         }
 
