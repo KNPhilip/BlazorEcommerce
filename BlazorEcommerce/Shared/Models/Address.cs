8b9aebba0832181791a6178020a5ff3b3f4866a5
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-03 10:29:00 +0200

    Created the Address model.

---
+++ BlazorEcommerce/Shared/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorEcommerce.Shared.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
--- BlazorEcommerce/Shared/Models/User.cs
+++ BlazorEcommerce/Shared/Models/User.cs
@@ -5,6 +5,10 @@
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName { get => FirstName + " " + LastName; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Address Address { get; set; }
     }
 }
