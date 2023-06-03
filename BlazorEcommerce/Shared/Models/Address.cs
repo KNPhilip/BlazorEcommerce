d6aba8be6ba7f50ed865f1b2387e5dccc155c107
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-03 10:30:18 +0200

    Implemented Address in the EcommerceContext.

--- BlazorEcommerce/Shared/Models/Address.cs
+++ BlazorEcommerce/Shared/Models/Address.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public User? User { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName { get => FirstName + " " + LastName; }
         public string Street { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
