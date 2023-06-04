ef09bd20031fd9ab78526e11990e0b2a263d97cf
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-04 18:11:35 +0200

    Added AddCategory method in the server-side CategoryService.

--- BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -12,6 +12,14 @@ namespace BlazorEcommerce.Server.Services.CategoryService
             _context = context;
         }
 
+        public async Task<ServiceResponse<List<Category>>> AddCategoryAsync(Category category)
+        {
+            category.Editing = category.IsNew = false;
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return await GetAdminCategoriesAsync();
+        }
+
         public async Task<ServiceResponse<List<Category>>> GetAdminCategoriesAsync()
         {
             var categories = await _context.Categories
--- BlazorEcommerce/Server/Services/CategoryService/ICategoryService.cs
+++ BlazorEcommerce/Server/Services/CategoryService/ICategoryService.cs
@@ -1,10 +1,9 @@
-﻿using BlazorEcommerce.Shared.Models;
-
-namespace BlazorEcommerce.Server.Services.CategoryService
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
 {
     public interface ICategoryService
     {
         Task<ServiceResponse<List<Category>>> GetCategoriesAsync();
         Task<ServiceResponse<List<Category>>> GetAdminCategoriesAsync();
+        Task<ServiceResponse<List<Category>>> AddCategoryAsync(Category category);
     }
 }
