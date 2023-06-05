4ee6a32832cd0cf2e45bc7f0d63148a3f78fbd39
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-05 10:58:31 +0200

    Implemented GetProductTypes method in the client-side ProductTypeService along with some other stuff.

--- BlazorEcommerce/Client/Services/ProductTypeService/IProductTypeService.cs
+++ BlazorEcommerce/Client/Services/ProductTypeService/IProductTypeService.cs
@@ -2,5 +2,8 @@
 {
     public interface IProductTypeService
     {
+        event Action OnChange;
+        public List<ProductType> ProductTypes { get; set; }
+        Task GetProductTypes();
     }
 }
--- BlazorEcommerce/Client/Services/ProductTypeService/ProductTypeService.cs
+++ BlazorEcommerce/Client/Services/ProductTypeService/ProductTypeService.cs
@@ -8,5 +8,16 @@
         {
             _http = http;
         }
+
+        public List<ProductType> ProductTypes { get; set; } = new List<ProductType>();
+
+        public event Action OnChange;
+
+        public async Task GetProductTypes()
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<ProductType>>>("api/producttype");
+            ProductTypes = result.Data;
+        }
     }
 }
