07d2fe36f5923b07fb9c8556e9bbb55a020b358d
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-05 10:56:01 +0200

    Created the ProductTypeService on the client-side.

---
+++ BlazorEcommerce/Client/Services/ProductTypeService/ProductTypeService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductTypeService
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly HttpClient _http;
+
+        public ProductTypeService(HttpClient http)
+        {
+            _http = http;
+        }
+    }
+}
