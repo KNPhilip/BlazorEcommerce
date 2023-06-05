dd1c87a1b432195e86105b5761b551a9c01c0f7b
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-05 10:49:24 +0200

    Created a ProductTypeService on the server-side.

---
+++ BlazorEcommerce/Server/Services/ProductTypeService/IProductTypeService.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductTypeService
+{
+    public interface IProductTypeService
+    {
+    }
+}
---
+++ BlazorEcommerce/Server/Services/ProductTypeService/ProductTypeService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductTypeService
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly EcommerceContext _context;
+
+        public ProductTypeService(EcommerceContext context)
+        {
+            _context = context;
+        }
+    }
+}
