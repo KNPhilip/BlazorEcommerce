0ea2c39bc3bd952872a7dd2204d5286815ab013c
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-18 10:16:34 +0200

    Modified the context

--- BlazorEcommerce/Server/Data/EcommerceContext.cs
+++ BlazorEcommerce/Server/Data/EcommerceContext.cs
@@ -387,5 +387,6 @@
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
