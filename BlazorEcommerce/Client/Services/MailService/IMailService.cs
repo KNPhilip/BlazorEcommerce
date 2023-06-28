e1048abb8b1b685c4f12dec708de34a1fb78459e
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-06-28 17:09:45 +0200

    Added the MailService to the client-side.

---
+++ BlazorEcommerce/Client/Services/MailService/IMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorEcommerce.Client.Services.MailService
+{
+    public interface IMailService
+    {
+    }
+}
---
+++ BlazorEcommerce/Client/Services/MailService/MailService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Client.Services.MailService
+{
+    public class MailService : IMailService
+    {
+        private readonly HttpClient _http;
+
+        public MailService(HttpClient http)
+        {
+            _http = http;
+        }
+    }
+}
