6c0060b917e8a197cc59e3ca4363d67662efbdbd
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-08-05 15:38:12 +0200

    Delete old projects

--- BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorEcommerce.Client.Services.OrderService
-{
-    public interface IOrderService
-    {
-        Task<string> PlaceOrder();
-        Task<List<OrderOverviewDto>> GetOrders();
-        Task<OrderDetailsDto> GetOrderDetails(int orderId);
-    }
-}
