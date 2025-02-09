6c4c4b1163daadf2c9afc92f90844d9d3378eaad
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-02-09 11:18:07 +0100

    Remove dead code (#83)

--- src/Domain/Interfaces/IAuthUIService.cs
+++ src/Domain/Interfaces/IAuthUIService.cs
@@ -2,11 +2,5 @@
 
 public interface IAuthUIService
 {
-    Task<bool> Register(UserRegisterDto request);
-    Task<string?> Login(UserLoginDto request);
-    Task<bool> ChangePassword(UserChangePasswordDto request);
-    Task<bool> ResetPassword(PasswordResetDto request);
-    Task<bool> ValidateResetPasswordToken(TokenValidateDto request);
-    Task<string?> CreateResetToken(ApplicationUser request);
     Task<bool> IsUserAuthenticated();
 }
